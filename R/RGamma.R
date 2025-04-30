a68dc57d4323e32e9f40edd8ca2a1d128102ca3b
sabrinabeyerNOAA <9096793+sabrinabeyerNOAA@users.noreply.github.com>
2025-04-30 11:49:33 -0700

    initial

--- R/RGamma.R
+++ R/RGamma.R
@@ -1,29 +1,28 @@
-RGamma <- 
-function(n, Mean, Var, nu = NULL, Shape.Scale = FALSE, verbose = FALSE)
-{
-        # Dispersion is 1/nu, where nu is the shape parameter.  Dispersion is equal to the CV squared for the gamma.
-
-         if(F) {  
-          # Example of generating data for a glm model with gamma errors.
-            x <- seq(1,20, len=50000)
-            y <- RGamma(50000, exp(3 + 2*x), 4*(exp(3 + 2*x))^2) # 4 is dispersion
-            y <- RGamma(50000, exp(3 + 2*x), (exp(3 + 2*x))^2, 4) # Now using the dispersion argument
-            summary(glm(y ~ x, family = Gamma(link='log'))) # Estimate of dispersion is given in summary()
-        }
-
-        if(is.null(nu))
-                nu <- Var/Mean^2
-
-        if(verbose) {
-                catf("\nAverage Shape =", mean(1/nu), "Average Scale =", mean(nu*Mean), "\n\n")
-                plot(x, nu*Mean, col='green', ylim=c(0, max(c(1/nu, nu*Mean))))
-                points(x, 1/nu)
-        }
-
-        if(Shape.Scale)
-          list(Shape= 1/nu, Scale = (nu*Mean))
-        else
-           rgamma(n, 1/nu, 1/(nu*Mean))
-}
- 
-
+rGamma <- function(n, Mean, Var, nu = NULL, Shape.Scale = FALSE, verbose = FALSE, gammaFunc = rgamma) {
+    
+            "  # Dispersion is 1/nu, where nu is the shape parameter.  Dispersion is equal to the CV squared for the gamma.  "
+       
+                if(F) {  
+                "  # Example of generating data for a glm model with gamma errors.  "
+                   x <- seq(1,20, len=50000)
+                   y <- rGamma(50000, exp(3 + 2*x), 4*(exp(3 + 2*x))^2) # 4 is Dispersion (nu = 1/Dispersion)
+                   summary(glm(y ~ x, family = Gamma(link='log'))) # Estimate of Dispersion is given in summary()
+                   y <- rGamma(50000, exp(3 + 2*x),  nu = 1/4) # Now using the nu shape argument, the variance argument is not used
+                   summary(glm(y ~ x, family = Gamma(link='log'))) 
+               }
+       
+               "  # Since Dispersion = CV^2 = Var/Mean^2 = 1/nu  " 
+               if(is.null(nu))
+                       nu <- Mean^2/Var
+       
+               if(verbose) {
+                       catf("\nAverage Shape =", mean(1/nu), "Average Scale =", mean(nu*Mean), "\n\n")
+                       plot(x, nu*Mean, col='green', ylim=c(0, max(c(1/nu, nu*Mean))))
+                       points(x, 1/nu)
+               }
+       
+               if(Shape.Scale)
+                 list(Shape= nu, Scale = Mean/nu)
+               else
+                 gammaFunc(n, nu, scale = Mean/nu)  # rgamma(n, shape, rate = 1, scale = 1/rate)
+       }
--- R/Setwd.R
+++ R/Setwd.R
@@ -1,7 +1,10 @@
-Setwd <- function(dir) {
 
-    HOME <<- getwd()
-    goHome <<- as.function(alist(setwd(HOME)))
-    setwd(dir)
+setWd <- function (text_for_copy_paste = FALSE) 
+{
+    "  # After filePath() returns out the machine dependent correct path, setwd() is used to change the working directory and getwd() verifies "
+    setwd(JRWToolBox::filePath())
+    if(text_for_copy_paste)
+        cat(paste0('\nsetwd("', getwd(), '")\n\n'))
+    else
+        print(getwd()) 
 }
-    
